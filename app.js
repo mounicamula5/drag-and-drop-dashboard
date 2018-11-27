10654bbc28d93d3f4a7bf7402178634635a1c9ae
mounicamula5 <44542329+mounicamula5@users.noreply.github.com>
2018-11-27 12:54:14 +0530

    deployed

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+ 
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
+ 
+app.set('views', __dirname + '/dist'); //
+ 
+// For all GET requests, send back index.html
+app.get('/', function(request, response) {
+  response.render('index'); // this index is from /dist/index
+});
+ 
+const port = process.env.PORT || 4200;
+app.set('port', port);
+ 
