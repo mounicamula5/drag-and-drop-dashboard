c801ffff4c6d3b0d7c377964f80f3712215a3de1
mounicamula5 <44542329+mounicamula5@users.noreply.github.com>
2018-11-27 13:36:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/drag-and-drop-dashboard'));
 // Start the app by listening on the default
 // Heroku port
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/drag-and-drop-dashboard/index.html'));
+});
 app.listen(process.env.PORT || 8080);
